a211e4bff10c0a9fa6a469c3eecca25e0ccb95ee
riotl <riotl@DESKTOP-V70I17V>
2024-04-23 11:18:46 +0800

    Add project files.

---
+++ PartyInvite/Controllers/InviteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvite.Models;
+namespace PartyInvite.Controllers
+{
+    public class InviteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: to store response of the guests
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
+           
+        }
+
+        public IActionResult GuestList()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
---
+++ PartyInvite/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PartyInvite.Models
+{
+    public class GuestResponse
+    {
+
+        [Required(ErrorMessage = "Please Enter your name")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your email")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Please Enter your mobile Number")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "Specify whether you'll attend the party")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
